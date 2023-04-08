7e77592de5aa87171248c7b3a4e0ea71d14c7835
Mike111222 <Tipu870@gmail.com>
2023-04-08 22:04:26 +0500

    JS Objects

---
+++ JSObject.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+  name: "Max",
+    legs: 4,
+    tails: 1,
+    friends: ["Buddy", "Charlie", "Daisy"]
+    // Only change code above this line
+  };
